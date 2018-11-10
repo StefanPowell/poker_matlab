6c8d7dd696b89cae81d57c0444cf6ad3e36e445b
StefanPowell <Stefan.Powell001@umb.edu>
2018-11-10 03:34:22 -0500

    Add files via upload

---
+++ poker.m
@@ -0,0 +1,52 @@
+deck = [2 3 4 5 6 7 8 9 10 11 12 13 14; 2 3 4 5 6 7 8 9 10 11 12 13 14; 2 3 4 5 6 7 8 9 10 11 12 13 14; 2 3 4 5 6 7 8 9 10 11 12 13 14]
+check_deck = [0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+player1_amount = 0 % total coins
+player2_amount = 0 % total coins
+player3_amount = 0 % total coins
+player4_amount = 0 % total coins
+players_hand = [0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0; 0 0] %stores each player hand
+bets = [0 0 0 0] %stores bets of each player
+
+%make sure value is not less than one and not greater than 5
+for j=1:4,
+   prompt = 'How much coins do you want to purchase ($1 - 5$)?';
+   coins = input(prompt)
+   if (j == 1)
+    player1_amount = coins
+   elseif (j == 2)
+    player2_amount = coins
+   elseif (j == 3)
+    player3_amount = coins
+   elseif (j == 4)
+   player4_amount = coins
+   end
+end
+
+%make sure same card not selected from deck twice
+count = 1
+for x=1:4,
+    for y=1:2
+        column_card_select = randi([1 4])
+        row_card_select = randi([2 14])
+        players_hand(count, 1) = column_card_select
+        players_hand(count, 2) = row_card_select
+        check_deck(column_card_select, row_card_select) = 1
+        count = count + 1
+    end
+end
+
+%place bets 
+%player 1 and two should not be hard coded
+prompt = 'What is the minimum bid';
+player_1_bet = input(prompt)
+if(player_1_bet < player1_amount)
+    bets(1,1) = player_1_bet
+    player1_amount = player1_amount - player_1_bet
+end
+prompt = 'What is the maximum bid';
+player_2_bet = input(prompt)
+if(player_2_bet < player2_amount)
+    bets(1,2) = player_2_bet
+    player2_amount = player2_amount - player_2_bet
+end
+
