2199e0c2edb26074b4a5d281acdf1fa1790fbe24
StefanPowell <Stefan.Powell001@umb.edu>
2018-11-17 18:55:48 -0500

    Update poker.m

--- poker.m
+++ poker.m
@@ -37,16 +37,44 @@ end
 
 %place bets 
 %player 1 and two should not be hard coded
+
 prompt = 'What is the minimum bid';
 player_1_bet = input(prompt)
 if(player_1_bet < player1_amount)
     bets(1,1) = player_1_bet
     player1_amount = player1_amount - player_1_bet
 end
+
 prompt = 'What is the maximum bid';
 player_2_bet = input(prompt)
 if(player_2_bet < player2_amount)
     bets(1,2) = player_2_bet
     player2_amount = player2_amount - player_2_bet
 end
+
+% prompt = 'Do you wish to call - C, fold = F, raise R';
+% player_3_bet = input(prompt)
+% switch player_3_bet
+%     case 'C'
+%         if(player_3_bet >= (player2_amount))
+%             bets(1,3) = player_3_bet
+%             player3_amount = player3_amount - player_3_bet
+%         else
+%             fold
+%         end
+%     case 'R'
+%         if(player_3_bet > (2*player2_amount))
+%             bets(1,3) = player_3_bet
+%             player3_amount = player3_amount - player_3_bet
+%         else
+%             call 
+%             fold
+%         end 
+%     otherwise                           %Fold
+%         
+% end
+
+
+%need to create function to change player each time
+
 
